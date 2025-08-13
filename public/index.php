c82181142b35f24acae268eff034f61cebeb2666
propanedreams <eirikssondk@yahoo.dk>
2025-08-13 13:12:20 +0200

    beginning of project

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Website</title>
+</head>
+<body>
+    
+
+
+    <Style>
+
+    </Style>
+
+    <Header>
+        <Div class="logo-holder">
+            <Img src="assets/images/logo.jpg"></Img>
+        </Div>
+        <Div class="header-div">
+            <div class="main-title">MUSIC WEBSITE</div>
+            <div class="main-nav"></div>
+                <div class="nav-item"><a href="">Home</a></div>
+                <div class="nav-item"><a href="">Music</a></div>
+                <div class="nav-item"><a href="">Category</a></div>
+                <div class="nav-item"><a href="">Artists</a></div>
+                <div class="nav-item"><a href="">About us</a></div>
+                <div class="nav-item"><a href="">Contact us</a></div>
+                <div class="nav-item"><a href="">Hi, User</a></div>
+        </Div>
+    </Header>
+
+</body>
+</html>
